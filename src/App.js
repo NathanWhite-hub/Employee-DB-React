81eb7565b48262f882d2e4e15859433165604da7
Nathan White <white.lnathan@gmail.com>
2022-11-12 16:29:19 -0500

    Add mapping, state, and images to employees.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [role, setRole] = useState("Dev");
+  const [employees, setEmployees] = useState([
+    {
+      name: "Bob",
+      role: "Intern",
+      img: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg",
+    },
+    {
+      name: "Nathan",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/10531636/pexels-photo-10531636.jpeg",
+    },
+    {
+      name: "Sarah",
+      role: "Engineer",
+      img: "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg",
+    },
+  ]);
   const showEmployees = true;
 
   return (
-    <div className="App bg-red-400">
+    <div className="App">
       {showEmployees ? (
         <>
           <input
@@ -17,9 +35,18 @@ function App() {
               setRole(e.target.value);
             }}
           />
-          <Employee name="Bob" role="Intern" />
-          <Employee name="Nathan" role={role} />
-          <Employee name="Sarah" />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              return (
+                <Employee
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
+          </div>
         </>
       ) : (
         <p>You cannot view the employees.</p>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,9 +1,21 @@
 function Employee(props) {
   return (
-    <>
-      <h3>Employee {props.name}</h3>
-      <p>{props.role ? props.role : "No Role"}</p>
-    </>
+    <div class="m-2 max-w-sm space-y-2 rounded-xl bg-white py-8 px-8 shadow-lg sm:flex sm:items-center sm:space-y-0 sm:space-x-6 sm:py-4">
+      <img
+        class="mx-auto block h-[100px] w-[100px] rounded-full object-cover sm:mx-0 sm:shrink-0"
+        src={props.img}
+        alt="Woman's Face"
+      />
+      <div class="space-y-2 text-center sm:text-left">
+        <div class="space-y-0.5">
+          <p class="text-lg font-semibold text-black">{props.name}</p>
+          <p class="font-medium text-slate-500">{props.role}</p>
+        </div>
+        <button class="rounded-full border border-purple-200 px-4 py-1 text-sm font-semibold text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+          Update
+        </button>
+      </div>
+    </div>
   );
 }
 
