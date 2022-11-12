0a77572d67e3398ad8ea50ad76f08be5cbcb2ef4
Nathan White <white.lnathan@gmail.com>
2022-11-12 15:47:34 -0500

    Hooks and Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
+  const [role, setRole] = useState("Dev");
+  const showEmployees = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello</p>
-      </header>
+      {showEmployees ? (
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <Employee name="Bob" role="Intern" />
+          <Employee name="Nathan" role={role} />
+          <Employee name="Sarah" />
+        </>
+      ) : (
+        <p>You cannot view the employees.</p>
+      )}
     </div>
   );
 }
