b83a4d3cb7be301aa6dbe988f298b30fcb07f44d
Nathan White <white.lnathan@gmail.com>
2022-11-14 17:16:08 -0500

    Edited modal buttons and styled form.

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -24,17 +24,61 @@ function EditEmployee() {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Updated Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          I will not close if you click outside me. Don't even try to press
-          escape key.
+          <form id="editmodal" class="w-full max-w-sm">
+            <div className="mb-6 md:flex md:items-center">
+              <div className="md:w-1/3">
+                <label
+                  className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0 md:text-right"
+                  for="inline-full-name"
+                >
+                  Full Name
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+                  id="name"
+                  type="text"
+                  value="Jane Doe"
+                />
+              </div>
+            </div>
+            <div className="mb-6 md:flex md:items-center">
+              <div className="md:w-1/3">
+                <label
+                  className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0 md:text-right"
+                  for="role"
+                >
+                  Role
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+                  id="role"
+                  type="text"
+                  value="Jane Doe"
+                />
+              </div>
+            </div>
+          </form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <button
+            className="rounded bg-red-500 py-2 px-4 font-bold text-white hover:bg-red-700"
+            onClick={handleClose}
+          >
             Close
-          </Button>
-          <Button variant="primary">Understood</Button>
+          </button>
+          <button
+            className="rounded bg-purple-600 py-2 px-4 font-bold text-white hover:bg-purple-700"
+            form="editmodal"
+          >
+            Update
+          </button>
         </Modal.Footer>
       </Modal>
     </>
