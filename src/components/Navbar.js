81b721c4ceb37ef63167f5a6c155bcf46d4f01fd
Nathan White <white.lnathan@gmail.com>
2022-11-15 14:13:00 -0500

    Added activate page functionality to navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { NavLink } from "react-router-dom";
 
 const navigation = [
-  { name: "Employees", href: "/employees", current: true },
-  { name: "Customers", href: "/customers", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
+  { name: "Employees", href: "/employees"},
+  { name: "Customers", href: "/customers" },
+  { name: "Projects", href: "#" },
+  { name: "Calendar", href: "#" },
 ];
 
 function classNames(...classes) {
@@ -35,19 +36,27 @@ function Navbar(props) {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <a
+                      <NavLink
                         key={item.name}
+                        to={item.href}
                         href={item.href}
-                        className={classNames(
+                        /*className={classNames(
                           item.current
                             ? "bg-gray-900 text-white no-underline"
                             : "text-gray-300 no-underline hover:bg-gray-700 hover:text-white",
                           "rounded-md px-3 py-2 text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
+                        )}*/
+                        className={({isActive}) => {
+                          return ( 
+                            'px-3 py-2 text-sm font-medium no-underline' + 
+                            (isActive
+                                ? ' bg-gray-900 text-white no-underline'
+                                : ' text-gray-300 no-underline hover:bg-gray-700 hover:text-white')
+                              );
+                            }} 
                       >
                         {item.name}
-                      </a>
+                      </NavLink>
                     ))}
                   </div>
                 </div>
