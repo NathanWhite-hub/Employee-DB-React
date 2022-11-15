3681235d93285de2baeb18adc588178291925827
Nathan White <white.lnathan@gmail.com>
2022-11-15 16:51:24 -0500

    Added useEffect functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import AddEmployee from "./components/AddEmployee";
 import EditEmployee from "./components/EditEmployee";
 import Navbar from "./components/Navbar";
 import Employees from "./pages/Employees";
 import Customers from "./pages/Customers";
+import Dictionary from "./pages/Dictionary";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar>
         <Routes>
-          <Route path="/employees" element={<Employees/>}/>
-          <Route path="/customers" element={<Customers/>}/>
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/dictionary" element={<Dictionary />} />
         </Routes>
       </Navbar>
     </BrowserRouter>
---
+++ src/pages/Dictionary.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function Dictionary() {
+  const [word, setWord] = useState("");
+  const [word2, setWord2] = useState("");
+
+  useEffect(() => {
+    console.log("State Updated ", word + " " + word2);
+  }, [word]);
+
+  return (
+    <>
+      <input
+        type="text"
+        onChange={(e) => {
+          setWord(e.target.value);
+        }}
+      />
+      <h2>Let's get the definition for {word}</h2>
+      <input
+        type="text"
+        onChange={(e) => {
+          setWord2(e.target.value);
+        }}
+      />
+      <h2>Let's get the definition for {word}</h2>
+    </>
+  );
+}
+
+export default Dictionary;
